1928a1d083af017f69efdaf99f55df7aa6e2ef76
falkTX <falktx@gmail.com>
2013-04-16 05:34:27 +0100

    Windows build

--- data/windows/build-win32.sh
+++ data/windows/build-win32.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 MINGW=i686-w64-mingw32
 MINGW_PATH=/opt/mingw32
 
@@ -20,7 +22,6 @@ export STRIP=$MINGW-strip
 export WINDRES=$MINGW-windres
 
 export PKG_CONFIG_PATH="`pwd`/data/windows:$MINGW_PATH/lib/pkgconfig"
-echo $PKG_CONFIG_PATH
 
 export WINEPREFIX=~/.winepy3
 
@@ -51,10 +52,16 @@ mv c++/*/*.exe data/windows
 
 rm -rf ./data/windows/Catarina
 rm -rf ./data/windows/Catia
-$CXFREEZE --include-modules=re --target-dir=".\\data\\windows\\Catarina" ".\\src\\catarina.py"
-$CXFREEZE --include-modules=re --target-dir=".\\data\\windows\\Catia" ".\\src\\catia.py"
+cp ./src/catarina.py ./src/catarina.pyw
+cp ./src/catia.py ./src/catia.pyw
+$CXFREEZE --include-modules=re --target-dir=".\\data\\windows\\Catarina" ".\\src\\catarina.pyw"
+$CXFREEZE --include-modules=re --target-dir=".\\data\\windows\\Catia" ".\\src\\catia.pyw"
+rm -f ./src/catarina.pyw
+rm -f ./src/catia.pyw
 
 cd data/windows
+mv cadence-jackmeter.exe    Cadence-JackMeter.exe
+mv cadence-xycontroller.exe Cadence-XYController.exe
 
 cp $WINEPREFIX/drive_c/windows/syswow64/python33.dll Catarina/
 cp $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/QtCore4.dll   Catarina/
@@ -62,15 +69,47 @@ cp $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/QtGui4.dll    Catarina/
 cp $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/QtOpenGL4.dll Catarina/
 cp $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/QtSvg4.dll    Catarina/
 cp $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/QtXml4.dll    Catarina/
+cp -r $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/plugins/imageformats/ Catarina/
+cp -r $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/plugins/iconengines/  Catarina/
 
 cp $WINEPREFIX/drive_c/windows/syswow64/python33.dll Catia/
 cp $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/QtCore4.dll   Catia/
 cp $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/QtGui4.dll    Catia/
 cp $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/QtOpenGL4.dll Catia/
 cp $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/QtSvg4.dll    Catia/
+cp -r $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/plugins/imageformats/ Catia/
+cp -r $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/plugins/iconengines/  Catia/
 
 rm -rf ./includes/
 rm -rf ./lib/
+
+# Build unzipfx
+make -C unzipfx-catarina -f Makefile.win32
+make -C unzipfx-catia -f Makefile.win32
+
+# Create static build
+rm -f Catarina.zip Catia.zip
+zip -r -9 Catarina.zip Catarina
+zip -r -9 Catia.zip Catia
+
+rm -f Catarina.exe Catia.exe
+cat unzipfx-catarina/unzipfx2cat.exe Catarina.zip > Catarina.exe
+cat unzipfx-catia/unzipfx2cat.exe Catia.zip > Catia.exe
+chmod +x Catarina.exe
+chmod +x Catia.exe
+
+# Cleanup
+make -C unzipfx-catarina -f Makefile.win32 clean
+make -C unzipfx-catia -f Makefile.win32 clean
+rm -f Catarina.zip Catia.zip
+rm -f unzipfx-*/*.exe
+
+# Final Zip
+rm -rf Cadence-0.8-beta2
+mkdir -p Cadence-0.8-beta2
+cp *.exe Cadence-0.8-beta2
+cp README Cadence-0.8-beta2
+zip -r -9 Cadence-0.8-beta2-win32.zip Cadence-0.8-beta2
 
 # Testing:
 echo "export WINEPREFIX=~/.winepy3"
