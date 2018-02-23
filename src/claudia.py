3e0fce81aceaf3b21c0ca06a5b9eb5d40a032013
falkTX <falktx@gmail.com>
2018-02-24 00:09:07 +0100

    Bump version

--- src/claudia.py
+++ src/claudia.py
@@ -2651,7 +2651,7 @@ class ClaudiaMainW(AbstractCanvasJackClass):
         QMessageBox.about(self, self.tr("About Claudia"), self.tr("<h3>Claudia</h3>"
                                                                   "<br>Version %s"
                                                                   "<br>Claudia is a Graphical User Interface to LADISH.<br>"
-                                                                  "<br>Copyright (C) 2010-2013 falkTX" % VERSION))
+                                                                  "<br>Copyright (C) 2010-2018 falkTX" % VERSION))
 
     def saveSettings(self):
         settings = QSettings()
