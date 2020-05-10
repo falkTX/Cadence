bf0869c111fd72e3a1caf804a7665dce9698632f
Gerion Entrup <gerion.entrup@flump.de>
2020-05-10 02:07:58 +0200

    catia/claudia: fix startup when a2j not present
    Consider this code:
    ```
    a = None
    print(a and a.b)
    ```
    This evaluates to None and not to False.
    
    Closes: #274

--- src/claudia.py
+++ src/claudia.py
@@ -1515,7 +1515,7 @@ class ClaudiaMainW(AbstractCanvasJackClass):
         if not gDBus.jack.IsStarted():
             self.ui.act_tools_a2j_start.setEnabled(False)
             self.ui.act_tools_a2j_stop.setEnabled(False)
-            self.ui.act_tools_a2j_export_hw.setEnabled(gDBus.a2j and not gDBus.a2j.is_started())
+            self.ui.act_tools_a2j_export_hw.setEnabled(bool(gDBus.a2j) and not gDBus.a2j.is_started())
         else:
             self.ui.act_tools_a2j_start.setEnabled(not started)
             self.ui.act_tools_a2j_stop.setEnabled(started)
