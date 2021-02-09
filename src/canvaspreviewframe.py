c44702b076da843722bb11dc7537b3e9a3700336
ivan tkachenko <me@ratijas.tk>
2021-02-09 20:36:55 +0300

    Revert the shebang stuff
    Per personal request in discussion of PR #306[1].
    
    [1]: https://github.com/falkTX/Cadence/pull/306#issuecomment-775951549

--- src/canvaspreviewframe.py
+++ src/canvaspreviewframe.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Custom Mini Canvas Preview, a custom Qt widget
--- src/clickablelabel.py
+++ src/clickablelabel.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Clickable Label, a custom Qt widget
